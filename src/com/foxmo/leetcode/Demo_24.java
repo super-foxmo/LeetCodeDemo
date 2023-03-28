c333f28a2536c7d7cafd7bf4006195da5cc5d5c0
super-foxmo <lixhui2000@icloud.com>
2023-03-28 21:22:12 +0800

    2023/03/28

--- src/com/foxmo/leetcode/Demo_24.java
+++ src/com/foxmo/leetcode/Demo_24.java
@@ -1,5 +1,7 @@
 package com.foxmo.leetcode;
 
+/*给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）*/
+
 public class Demo_24 {
     public ListNode swapPairs(ListNode head) {
         if(head == null || head.next == null){
