f0a61bb97a5580464ce0b96ae6c40823b99ef591
super-foxmo <lixhui2000@icloud.com>
2023-05-04 16:44:14 +0800

    2023/05/04

--- src/com/foxmo/leetcode/backtrack/Demo_40_组合总和2.java
+++ src/com/foxmo/leetcode/backtrack/Demo_40_组合总和2.java
@@ -5,16 +5,16 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Demo_39_组合总和 {
+public class Demo_40_组合总和2 {
     static LinkedList<Integer> list = new LinkedList<>();
     static List<List<Integer>> lists = new ArrayList<>();
     static int count = 0;
 
     public static void main(String[] args) {
-        combinationSum(new int[]{2,3,5},8);
+        combinationSum2(new int[]{10,1,2,7,6,1,5},8);
     }
 
-    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         backtrack(candidates,target,0);
         for (int i = 0; i < lists.size(); i++) {
@@ -28,8 +28,6 @@ public class Demo_39_组合总和 {
         if (count == target) {      //决定深度
             lists.add(new ArrayList<>(list));
             return;
-        }else if (count > target){
-            return;
         }
 
         for (int i = startIndex; i < candidates.length; i++) {      //决定宽度
@@ -38,18 +36,23 @@ public class Demo_39_组合总和 {
                 count -= candidates[i];
                 break;
             }
+            if (lists.size() > 0){
+                boolean flag = true;
+                for (int j = 0; j < list.size(); j++) {
+                    if (lists.get(lists.size() - 1).size() <= j || lists.get(lists.size() - 1).get(j) != list.get(j)){
+                        flag = false;
+                    }
+                }
+                if (flag && lists.get(lists.size() - 1).get(list.size()) == candidates[i]){
+                    count -= candidates[i];
+                    continue;
+                }
+            }
+
             list.add(candidates[i]);
-            backtrack(candidates,target,i);
+            backtrack(candidates,target,i + 1);
             count -= candidates[i];
             list.removeLast();
         }
-    }
-
-    public static int getCount(List<Integer> list){
-        int count = 0;
-        for (int i = 0; i < list.size(); i++) {
-            count += list.get(i);
-        }
-        return count;
     }
 }
