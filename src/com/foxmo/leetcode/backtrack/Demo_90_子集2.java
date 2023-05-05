55fa485f9997ec1d8fb17437aca05fd1010dc359
super-foxmo <lixhui2000@icloud.com>
2023-05-05 23:59:39 +0800

    2023/05/05

--- src/com/foxmo/leetcode/backtrack/Demo_90_子集2.java
+++ src/com/foxmo/leetcode/backtrack/Demo_90_子集2.java
@@ -1,35 +1,40 @@
 package com.foxmo.leetcode.backtrack;
 
-import com.sun.org.apache.bcel.internal.generic.FSUB;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Demo_78_子集 {
+public class Demo_90_子集2 {
     static LinkedList<Integer> list = new LinkedList<>();
     static List<List<Integer>> lists = new ArrayList<>();
 
     public static void main(String[] args) {
-        subsets(new int[]{1,1,2,2});
+        subsetsWithDup(new int[]{1,1,2,2});
         for (int i = 0; i < lists.size(); i++) {
             System.out.println(lists.get(i));
         }
 
     }
 
-    public static List<List<Integer>> subsets(int[] nums) {
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
         lists.add(new ArrayList<>());
-        backtrack(nums,0);
+        backtrack(nums,0,new boolean[nums.length]);
         return lists;
     }
 
-    public static void backtrack(int[] nums,int startIndex){
+    public static void backtrack(int[] nums,int startIndex,boolean[] used){
         for (int i = startIndex; i < nums.length; i++) {
+            //去重
+            if (i > 0 && nums[i] == nums[i - 1] && used[i-1] == false){
+                break;
+            }
+            used[i] = true;
             list.add(nums[i]);
             lists.add(new ArrayList<>(list));
-            backtrack(nums,i + 1);
+            backtrack(nums,i + 1,used);
+            used[i] = false;
             list.removeLast();
         }
     }
+
 }
