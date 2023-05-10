3586312785e78e9e364242549944b81c61208a9a
super-foxmo <lixhui2000@icloud.com>
2023-05-11 00:00:51 +0800

    2023/05/11

---
+++ src/com/foxmo/leetcode/dynamic_plan/Demo_322_零钱兑换.java
@@ -0,0 +1,25 @@
+package com.foxmo.leetcode.dynamic_plan;
+
+public class Demo_322_零钱兑换 {
+    public static void main(String[] args) {
+        System.out.println(coinChange(new int[]{2}, 3));
+    }
+    public static int coinChange(int[] coins, int amount) {
+        //初始化dp
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                if (dp[j - coins[i]] != Integer.MAX_VALUE){
+
+                    dp[j] = Math.min(dp[j],dp[j - coins[i]] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
